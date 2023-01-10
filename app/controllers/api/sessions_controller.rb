60775b67d2ec0cc716c8b10588f94fb22bb80dc7
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-01-09 16:32:30 -0800

    namespacing

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,13 +1,12 @@
-class SessionsController < ApplicationController
-
-  def create 
+class Api::SessionsController < ApplicationController
+  def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json :user, status: :created
     end
-  else 
-    render json: { errors: ["Invalid Username or Password"] }, status: :unauthorized
+  else
+    render json: { errors: ["Invalid Email or Password"] }, status: :unauthorized
   end
 
   def destroy
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def show
+    render json: @current_user
+  end
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation, :name, :address, :phone, :admin)
+  end
+end
