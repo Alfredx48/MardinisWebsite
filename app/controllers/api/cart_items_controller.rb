a32b6f2f627819d6b7f911b059dc3a18679f8b2c
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-01-16 13:41:56 -0800

    render

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,14 +1,19 @@
 class Api::CartItemsController < ApplicationController
-  skip_before_action :authorize, only: [:destroy]
-    before_action :set_cart_item, only: [:destroy]
+  skip_before_action :authorize, only: [:destroy, :index]
+  before_action :cart_item, only: [:destroy]
+
+  def index 
+    render json: CartItem.all
+  end
+
   def destroy
-    @cart_item.destroy
+    cart_item.destroy
     render json: { message: "Item removed from cart." }
   end
 
   private
 
-  def set_cart_item
-    @cart_item = CartItem.find(params[:id])
+  def cart_item
+    cart_item = CartItem.find(params[:id])
   end
 end
