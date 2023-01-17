1301ac2beecdb9bde2289845ed9729678036bd24
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-01-17 10:40:52 -0800

    fixed issue with total oer 1000 in cart, cam helped

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -3,19 +3,23 @@ import { useNavigate } from "react-router-dom";
 import CartItems from "./CartItems";
 
 function Cart({ cartId, setCartId, currentUser, cart, setCart }) {
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+	})
 	const navigate = useNavigate();
 	const calculateTotals = (cart) => {
 		let cart_total = 0;
 		let cart_items = 0;
 		if (cart && cart.cart_items && cart.cart_items.length > 0) {
 			cart.cart_items.forEach((item) => {
-				cart_total += parseFloat(item.item_total.replace("$", ""));
+				cart_total += parseFloat(item.item_total.replace("$", "").replace(",", ""))
 				cart_items += item.quantity;
 			});
 		}
-		return { cart_total: cart_total.toFixed(2), cart_items };
+		return { cart_total: formatter.format(cart_total), cart_items };
 	};
-
+	
 	useEffect(() => {
 		const storedCartId = localStorage.getItem("cartId")
 			? localStorage.getItem("cartId")
@@ -74,7 +78,7 @@ function Cart({ cartId, setCartId, currentUser, cart, setCart }) {
 			<button onClick={() => navigate("/order-now")}>add more items</button>
 			{mappedCartItems()}
 			{cart.cart_items && cart.cart_items.length > 0 ? (
-				<h4>Total Cost: ${cart_total}</h4>
+				<h4>Total Cost: {cart_total}</h4>
 			) : null}
 			{cart.cart_items && cart.cart_items.length > 0 ? (
 				<h4> Total items: {cart_items}</h4>
