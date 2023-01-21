d3ae348c86fdd4ec7bb6c08565ca56df63933ce7
Alfred Shaheen <shaheenalfred48@gmail.com>
2023-01-21 14:43:17 -0800

    render

--- client/src/components/PendingOrders.js
+++ client/src/components/PendingOrders.js
@@ -5,13 +5,18 @@ import moment from 'moment';
 
 function PendingOrders({ order }) {
     const format = "MMMM Do YYYY, h:mm a";
-    const date = moment(order.created_at).format(format);
+  const date = moment(order.created_at).format(format);
+  
+  const formatter = new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+	})
 
     
   return (
     <div>
       <h1>{order.status}</h1>
-      <p>{order.total_cost}</p>
+      <p>{formatter.format(order.total_cost)}</p>
       <p>{order.total_items}</p>
       <p>{date}</p>
 
